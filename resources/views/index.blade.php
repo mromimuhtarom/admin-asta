b24965cb35ebd51f85ea192102231babd4b87567
mromimuhtarom28@gmail.com <mromimuhtarom28@gmail.com>
2019-05-23 16:31:25 +0700

    tambahan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,7 +25,11 @@
     <!-- bootstrap -->
     {{-- <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet"> --}}
     <link rel="stylesheet" href="/css/x-editable.css">
-    <script src="http://code.jquery.com/jquery-2.0.3.min.js"></script> 
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.5.6/css/buttons.dataTables.min.css">
+    <script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script>   
     <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>  
     {{-- <script src="/js/dropzone.js"></script> --}}
 
