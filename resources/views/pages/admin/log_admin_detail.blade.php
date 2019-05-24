81d53fe56c04fbf3a1a3be404692a013ef2f6f4e
mromimuhtarom28@gmail.com <mromimuhtarom28@gmail.com>
2019-05-24 10:21:18 +0700

    image gift

---
+++ resources/views/pages/admin/log_admin_detail.blade.php
@@ -0,0 +1,189 @@
+@extends('index')
+
+@section('sidebarmenu')
+    @include('menu.menuadmin')
+@endsection
+
+@section('content')
+    <link rel="stylesheet" href="/css/admin.css">
+
+      <!-- Modal -->
+  <div class="modal fade" id="basicExampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header" style="margin-top:5%;">
+        <h5 class="modal-title" id="exampleModalLabel">Create Role Admin</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form action="" method="POST">
+        {{  csrf_field() }}
+      <div class="modal-body">
+        <input type="text" name="rolename" placeholder="Role Name" required>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Save changes</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+@if (count($errors) > 0)
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all as $error)
+        <li>{{$error}}</li>  
+        @endforeach
+    </ul>
+</div>
+    
+@endif
+
+@if (\Session::has('success'))
+    <div class="alert alert-success">
+        <p>{{\Session::get('success')}}</p>
+    </div>
+    
+@endif
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header" style="margin-top:5%;">
+          <h5 class="modal-title" id="exampleModalLabel">Delete Data</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          Are You Sure Want To Delete It
+          <form action="{{ route('UserAdmin-delete') }}" method="post">
+            {{ method_field('delete')}}
+            {{ csrf_field() }}
+            <input type="hidden" name="roleid" id="roleid" value="">
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="button_example-yes">Yes</button>
+          <button type="button" class="button_example-no" data-dismiss="modal">No</button>
+        </div>
+          </form>
+      </div>
+    </div>
+  </div>
+    {{-- end delete notification --}}
+
+
+
+
+    <div class="table-aii">
+        <div class="footer-table">
+              <button type="button" class="btn btn-primary add-btn" data-toggle="modal" data-target="#basicExampleModal">
+                  <i class="fas fa-plus-circle"></i>Create Role Admin
+              </button>
+        </div>
+         <table id="dt-material-checkbox" class="table table-striped" style="margin-left:1px;margin-top:-5%;" cellspacing="0" width="100%">
+            <thead class="th-table">
+              <tr>
+                <th></th>
+                <th class="th-sm">Image</th>
+                <th class="th-sm">Role</th>
+                <th class="th-sm">Action</th>
+                <th style="width:2px;"></th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach($roles as $role)
+                <tr>
+                    <td><input type="checkbox" name="deletepermission" class="deletepermission{{ $role->role_id }}"></td>
+                    <td></td>
+                    <td>{{ $role->name }}</td>
+                    <td><a href="#" class="myButton">View & Edit</a></td>
+                    <td><a href="#" style="color:red;" class="delete{{ $role->role_id }}" id="delete" data-pk="{{ $role->role_id }}" data-toggle="modal" data-target="#delete"><i class="fas fa-times"></i></a></td>
+                </tr>
+                @endforeach
+            </tbody>
+            {{-- <tfoot>
+                <tr>
+                    <th>Name
+                    </th>
+                    <th>Position
+                    </th>
+                    <th>Office
+                    </th>
+                    <th colspan="2">Age
+                    </th>
+                    <th colspan="2">Start date
+                    </th>
+                </tr>
+            </tfoot> --}}
+          </table>
+         
+      </div>    
+
+      <script>
+      $(document).ready(function() {
+        $.ajaxSetup({
+          headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+          }
+        });
+
+        // $('#selecctall').click(function(event) { 
+        //       if(this.checked) { // check select status
+        //         $('.deletepermission').each(function() { 
+        //           this.checked = true;  //select all 
+        //         });
+        //       }else{
+        //         $('.deletepermission').each(function() { 
+        //           this.checked = false; //deselect all             
+        //         });        
+        //       }
+        // });
+      
+      });
+      table = $('#dt-material-checkbox').dataTable({
+          columnDefs: [{
+          orderable: false,
+          className: 'select-checkbox',
+          targets: 0
+          }],
+          "pagingType": "full_numbers",
+          "bInfo" : false,
+          "sDom": '<"row view-filter w-50 add"<"col-sm-12"<"pull-right border-left margin-left"l><"pull-right margin-left"f><"clearfix">>>t<"row view-pager"<"col-sm-12"<"bottom"p>>>',
+          select: {
+          style: 'os',
+          selector: 'td:first-child'
+          },
+          "fnRowCallback": function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+              $.ajaxSetup({
+                headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+              });
+
+              $('.usertext').editable({
+                mode :'popup'
+              });
+             
+              $('.category').editable({
+                //value: 'drink',
+                source: [
+                  {value: 'drink', text: 'Drink'},
+                  {value: 'food', text: 'Food'},
+                  {value: 'emoji', text: 'Emoji'},
+                ]
+              }); 
+    
+          }
+      });
+</script>
+
+      
+@endsection
--- resources/views/pages/store/Gift.blade.php
+++ resources/views/pages/store/Gift.blade.php
@@ -13,11 +13,11 @@
 	position: relative;
 	display: inline-block;
 	margin: auto;
-  /* border-radius: 50%; */
+  border-radius: 50%;
   border: 1px solid black;
 	overflow: hidden;
-	width: 100%;
-	height: 100%;
+	width: 100px;
+	height: 100px;
 	/* vertical-align: middle */
 }
 	.media-overlay {
@@ -44,16 +44,17 @@
 			font-size: 2em;
 			height: 100%;
 			line-height: 100px;
-			position: absolute;
+      position: absolute;
 			z-index: 0;
 			width: 100%;
 			text-align: center;
 		}
 	.media-object {}
 		.img-object {
-			/* border-radius: 50%; */
-			width: 100%;
-			height: 100%;
+      border: 1px solid black;
+			border-radius: 50%;
+			width: 100px;
+			height: 100px;
 			display: block;
 		}
 
@@ -174,7 +175,7 @@ echo 'function Upload'.$gf->id.'() {';
                                 <i class="fas fa-edit media-icon"></i>
                               </span>
                               <figure class="media-object">
-                                <img class="img-object imgupload{{ $gf->id }}" src="/images/gifts/{{ $gf->image_Url }}">
+                                <img class="img-object imgupload{{ $gf->id }}" src="/images/gifts/{{ $gf->image_url }}">
                               </figure>
                             </div>
                             <div class="media-control">
@@ -188,7 +189,7 @@ echo 'function Upload'.$gf->id.'() {';
                     <td><a href="#" class="usertext" data-name="chipsPrice" data-title="Chip Price" data-pk="{{ $gf->id }}" data-type="number" data-url="{{ route('GiftStore-update') }}">{{ $gf->chipsPrice }}</a></td>
                     {{-- <td><a href="#" class="usertext" data-name="diamondPrice" data-title="Gold Price" data-pk="{{ $gf->id }}" data-type="number" data-url="{{ route('GiftStore-update') }}">{{ $gf->diamondPrice }}</a></td>
                     <td><a href="#" class="usertext" data-name="expire" data-title="expire" data-pk="{{ $gf->id }}" data-type="number" data-url="{{ route('GiftStore-update') }}">{{ $gf->expire }}</a></td> --}}
-                    <td><a href="#" class="category" data-name="category" data-pk="{{ $gf->id }}" data-type="select" data-value="{{ $gf->category_id }}" data-url="{{ route('GiftStore-update') }}" data-title="Select type">{{ $gf->category_id }}</a></td>
+                    <td><a href="#" class="category" data-name="category_id" data-pk="{{ $gf->id }}" data-type="select" data-value="{{ $gf->category_id }}" data-url="{{ route('GiftStore-update') }}" data-title="Select type">{{ $gf->strCategory() }}</a></td>
                     <td></td>
                 </tr>
                 @endforeach
@@ -224,9 +225,9 @@ echo 'function Upload'.$gf->id.'() {';
               $('.category').editable({
                 value: 0,
 				        source: [
-					        {value: 'WIN_HIGHCARD', text: 'High Card'},
-					        {value: 'WIN_PAIR', text: 'Pair'},
-					        {value: 'WIN_2PAIR', text: '2 pair'},
+					        {value: '1', text: 'Makanan'},
+					        {value: '2', text: 'Minuman'},
+					        {value: '3', text: 'Item'},
                 ]
               });
 
