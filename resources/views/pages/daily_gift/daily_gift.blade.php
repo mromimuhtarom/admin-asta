c8c72ff6e73c760268d3834de2d575c5da53baff
clive <inthenameofclive@gmail.com>
2019-06-12 11:03:06 +0700

    daily-gift

--- resources/views/pages/daily_gift/daily_gift.blade.php
+++ resources/views/pages/daily_gift/daily_gift.blade.php
@@ -7,113 +7,218 @@
 
 @section('content')
 
+@if (count($errors) > 0)
+  <div class="alert alert-danger">
+    <ul>
+      @foreach ($errors->all as $error)
+      <li>{{$error}}</li>  
+      @endforeach
+    </ul>
+  </div>
+      
+@endif
+  
+@if (\Session::has('success'))
+  <div class="alert alert-success">
+    <p>{{\Session::get('success')}}</p>
+  </div>
+@endif
 
-      <!-- Modal -->
-      <div class="modal fade" id="basicExampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-      aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header" style="margin-top:5%;">
-            <h5 class="modal-title" id="exampleModalLabel">Create Daily Gift</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
+<!-- Table -->
+<div class="jarviswidget jarviswidget-color-blue-dark no-padding" id="wid-id-18" data-widget-colorbutton="false" data-widget-editbutton="false">
+
+  <header>
+    <div class="widget-header">	
+      <h2><strong>Daily Gift</strong></h2>				
+    </div>
+  </header>
+
+  <div>
+    <div class="widget-body">
+      <div class="widget-body-toolbar">
+        
+        <div class="row">
+          
+          <!-- Button tambah chip store baru -->
+          <div class="col-9 col-sm-5 col-md-5 col-lg-5">
+            <div class="input-group">
+              @if($menu)
+              <button class="btn sa-btn-primary" data-toggle="modal" data-target="#createGoldStore">
+                <i class="fa fa-plus"></i>
+              </button>
+              @endif
+            </div>
           </div>
-          <form action="" method="POST">
-            {{  csrf_field() }}
-          <div class="modal-body">
-            <input type="text" name="title" placeholder="Title" required><br>
-            <input type="text" name="quantity" placeholder="Quantity"><br>
-            <select name="action">
-              <option>Select Action</option>
-              <option>Chip</option>
-              <option>Gold</option>
+          <!-- End Button tambah chip store baru -->
+
+        </div>
+
+      </div>
+      
+      <div class="custom-scroll table-responsive" style="max-height:600px;">
+        
+        <div class="table-outer">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                @if($menu)
+                  <th class="th-sm"></th>
+                @endif
+                <th class="th-sm">Title Gift</th>
+                <th class="th-sm">Category</th>
+                <th class="th-sm">Quantity</th>
+                <th class="th-sm">Status</th>
+                @if($menu)
+                  <th>Action</th>
+                @endif
+              </tr>
+            </thead>
+            <tbody>
+              {{-- @foreach($guests as $gs) --}}
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              {{-- @endforeach --}}
+            </tbody>
+          </table>
+        </div>
+      
+      </div>
+    
+    </div>
+  </div>
+</div>
+<!-- end Table -->
+
+<!-- Modal -->
+<div class="modal fade" id="createGoldStore" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" id="myModalLabel">Create New Daily Gift</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+          ×
+        </button>
+      </div>
+      <form action="{{ route('GoldStore-create') }}" method="post">
+        @csrf
+        <div class="modal-body">
+          <div class="form-group">
+            <input type="text" name="title" class="form-control" id="basic-url" placeholder="title" required>
+          </div>
+          <div class="form-group">
+            <input type="number" name="quantity" class="form-control" id="basic-url" placeholder="quantity" required>
+          </div>
+          <div class="form-group">
+            <select class="custom-select">
+              <option selected>Select Action</option>
+              <option value="1">Chip</option>
+              <option value="2">Gold</option>
             </select>
           </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="submit" class="btn btn-primary">Save changes</button>
-          </div>
-          </form>
         </div>
-      </div>
-    </div>
-    
-    @if (count($errors) > 0)
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all as $error)
-            <li>{{$error}}</li>  
-            @endforeach
-        </ul>
-    </div>
-        
-    @endif
-    
-    @if (\Session::has('success'))
-        <div class="alert alert-success">
-            <p>{{\Session::get('success')}}</p>
+        <div class="modal-footer">
+          <button type="submit" class="btn btn-default" data-dismiss="modal">
+            Cancel
+          </button>
+          <button type="submit" class="btn sa-btn-primary">
+            Save
+          </button>
         </div>
-        
-    @endif
-
-
-<div class="table-aii">
-    <div class="footer-table">
-                        <button type="button" class="btn btn-primary add-btn" data-toggle="modal" data-target="#basicExampleModal">
-                          <i class="fas fa-plus-circle"></i>Create Daily Gift
-                        </button>
+      </form>
     </div>
-     <table id="dt-material-checkbox" class="table table-striped" style="margin-left:1px;margin-top:-5%;" cellspacing="0" width="100%">
-        <thead class="th-table">
-          <tr>
-            <th></th>
-            <th class="th-sm">Title Gift</th>
-            <th class="th-sm">Category</th>
-            <th class="th-sm">Quantity</th>
-            <th class="th-sm">Status</th>
-          </tr>
-        </thead>
-        <tbody>
-            {{-- @foreach($guests as $gs) --}}
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            {{-- @endforeach --}}
-        </tbody>
-      </table>
-     
+  </div>
 </div>
-<script>
-    table = $('#dt-material-checkbox').dataTable({
-          columnDefs: [{
-          orderable: false,
-          className: 'select-checkbox',
-          targets: 0
-          }],
-          "pagingType": "full_numbers",
-          "bInfo" : false,
-          "sDom": '<"row view-filter w-50 add"<"col-sm-12"<"pull-right border-left margin-left"l><"pull-right margin-left"f><"clearfix">>>t<"row view-pager"<"col-sm-12"<"bottom"p>>>',
-          select: {
-          style: 'os',
-          selector: 'td:first-child'
-          },
-          "fnRowCallback": function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
-              $.ajaxSetup({
-                headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                }
-              });
+<!-- end Modal -->
 
-              $('.usertext').editable({
-                mode :'popup'
-              });
-    
-          }
+<!-- delete Modal -->
+<div class="modal fade" id="delete-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header" style="margin-top:5%;">
+        <h5 class="modal-title" id="exampleModalLabel">Delete Data</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          × 
+        </button>
+      </div>
+      <div class="modal-body">
+        Are You Sure Want To Delete It
+        {{-- <form action="{{ route('GoldStore-delete') }}" method="post">
+          {{ method_field('delete')}}
+          {{ csrf_field() }}
+          <input type="hidden" name="userid" id="userid" value="">
+      </div>
+      <div class="modal-footer">
+        <button type="submit" class="button_example-yes">Yes</button>
+        <button type="button" class="button_example-no" data-dismiss="modal">No</button>
+      </div>
+        </form> --}}
+    </div>
+  </div>
+</div>
+<!-- End delete Modal -->
+  
+<!-- script -->
+<script>
+  $(document).ready(function() {
+    $('table.table').dataTable( {
+      "lengthMenu": [[20, 25, 50, -1], [20, 25, 50, "All"]],
     });
-</script>    
+  });
+
+  table = $('table.table').dataTable({
+    "sDom": "t"+"<'dt-toolbar-footer d-flex test'>",
+    "autoWidth" : true,
+    "paging": false,
+    "classes": {
+      "sWrapper": "dataTables_wrapper dt-bootstrap4"
+    },
+    "oLanguage": {
+      "sSearch": '<span class="input-group-addon"><i class="fa fa-search"></i></span>'
+    },
+    "fnRowCallback": function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+      $.ajaxSetup({
+        headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+      });
+
+      $('.usertext').editable({
+        mode :'inline'
+      });
+
+      // delete gold store
+      // @php
+      //   foreach($getGolds as $gold) {
+      //     echo'$(".delete'.$gold->id.'").hide();';
+      //     echo'$(".deletepermission'.$gold->id.'").on("click", function() {';
+      //       echo 'if($( ".deletepermission'.$gold->id.':checked" ).length > 0)';
+      //       echo '{';
+      //         echo '$(".delete'.$gold->id.'").show();';
+      //       echo'}';
+      //       echo'else';
+      //       echo'{';
+      //         echo'$(".delete'.$gold->id.'").hide();';
+      //       echo'}';
+
+      //     echo '});';
+        
+      //     echo'$(".delete'.$gold->id.'").click(function(e) {';
+      //       echo'e.preventDefault();';
+
+      //       echo"var id = $(this).attr('data-pk');";
+      //       echo'var test = $("#userid").val(id);';
+      //     echo'});';
+      //   }
+      // @endphp
+    },
+    responsive: true
+  });
+</script>
+<!-- end script -->
 @endsection
