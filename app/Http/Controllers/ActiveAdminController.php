c28dc58aeb9b08a5c994e7d6b412001a8f48d392
mromimuhtarom28@gmail.com <mromimuhtarom28@gmail.com>
2019-07-12 13:28:48 +0700

    ketinggalan query di active admin

--- app/Http/Controllers/ActiveAdminController.php
+++ app/Http/Controllers/ActiveAdminController.php
@@ -14,7 +14,7 @@ class ActiveAdminController extends Controller
         $date->modify('-2 minutes');
 
         $active = OperatorActive::join('asta_db.operator', 'asta_db.operator.op_id', '=', 'asta_db.operator_active.op_id')
-                  ->select('asta_db.operator', 
+                  ->select('asta_db.operator.username', 
                            'asta_db.operator_active.date_login', 
                            'asta_db.operator_active.ip'
                     )
