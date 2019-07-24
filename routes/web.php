65d9174368e2f41ec7408ec7fe408ac04ff101df
mromimuhtarom28@gmail.com <mromimuhtarom28@gmail.com>
2019-07-24 08:28:54 +0700

    cache:clear,config:clear & route.web

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,37 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-// return view('login');
-// Route::get('/', function () {    })->middleware('home');
-// Route::middleware('')
 Route::get('/', ['uses' => 'LoginController@loginbefore', 'middleware' => 'home'])->name('login');
 Route::post('login', 'LoginController@login')->name('login');
 Route::get('/avatars/{avatar}', 'PlayersController@avatar')->name('imageAvatar');
 
-Route::get('/datatables', function () {
-    return view('design.datatables');
+
+
+ //Clear route cache:
+ Route::get('/route-clear', function() {
+    $exitCode = Artisan::call('route:clear');
+    return 'Routes cache cleared';
+});
+Route::get('/config-clear', function() {
+    $exitCode = Artisan::call('config:clear');
+    return 'Routes cache cleared';
+});
+
+//Clear config cache:
+Route::get('/config-cache', function() {
+    $exitCode = Artisan::call('config:cache');
+    return 'Config cache cleared';
+}); 
+
+// Clear application cache:
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('cache:clear');
+    return 'Application cache cleared';
+});
+
+// Clear view cache:
+Route::get('/view-clear', function() {
+    $exitCode = Artisan::call('view:clear');
+    return 'View cache cleared';
 });
 Route::middleware('authenticated')->group(function(){
     Route::group(['prefix' => 'Edit-Profile'], function(){
