cc5dcdbcd2c0628409c8213ec711a2765dfe754d
mromimuhtarom28@gmail.com <mromimuhtarom28@gmail.com>
2019-08-26 11:20:01 +0700

    athenticated database

--- app/Http/Middleware/Authenticated.php
+++ app/Http/Middleware/Authenticated.php
@@ -62,7 +62,7 @@ class Authenticated
             } else {
                 if($cahce_op)
                 {
-                    Db::table('operator_active')->update([
+                    Db::table('asta_db.operator_active')->update([
                         'op_id'       => $op,
                         'date_update' => Carbon::now('GMT+7'),
                         'ip'          => request()->ip()
